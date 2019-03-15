d5e4214221fef14b8cc7b5182a94cfe052573d46
ruchir_sachdeva <ruchirsachdeva@gmail.com>
2019-03-15 10:37:58 +0100

    fixed testsession repo methods

--- src/main/java/com/lnu/foundation/repository/TestSessionRepository.java
+++ src/main/java/com/lnu/foundation/repository/TestSessionRepository.java
@@ -2,9 +2,11 @@ package com.lnu.foundation.repository;
 
 import com.lnu.foundation.model.TestSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -29,16 +31,19 @@ public interface TestSessionRepository extends JpaRepository<TestSession, Long> 
     @RestResource(path = "byTherapyId", rel = "byTherapyId")
     Collection<TestSession> findByTherapy_TherapyId(@Param("id") Long id);
 
-    @RestResource(path = "byTherapyIdRequested", rel = "byTherapyIdRequested")
-    Collection<TestSession> findByTherapy_TherapyIdAndDuration_StartTimeIsNull(@Param("id") Long id);
+    @RestResource
+    @Query("select t from TestSession t left join t.therapy th where th.therapyId = ?1 and t.duration.startTime is not null")
+    Collection<TestSession> byTherapyIdRequested(@Param("id") Long id);
 
-    @RestResource(path = "byTherapyIdUpcoming", rel = "byTherapyIdUpcoming")
-    Collection<TestSession> findByTherapy_TherapyIdAndDuration_StartTimeNotNullAndDuration_StartTimeGreaterThan(@Param("id") Long id, @Param("currentDate") LocalDateTime currentDate);
+    @RestResource
+    @Query("select t from TestSession t left join t.therapy th where th.therapyId = ?1 and t.duration.startTime is not null and t.duration.startTime >= CURRENT_DATE ")
+    Collection<TestSession> byTherapyIdUpcoming(@Param("id") Long id);
 
-    @RestResource(path = "byTherapyIdHistory", rel = "byTherapyIdHistory")
-    Collection<TestSession> findByTherapy_TherapyIdAndDuration_StartTimeNotNullAndDuration_StartTimeLessThan(@Param("id") Long id, @Param("currentDate") LocalDateTime currentDate);
+    @RestResource
+    @Query("select t from TestSession t left join t.therapy th where th.therapyId = ?1 and t.duration.startTime is not null and t.duration.startTime < CURRENT_DATE ")
+    Collection<TestSession> byTherapyIdHistory(@Param("id") Long id);
 
     @RestResource(path = "byMedUpcoming", rel = "byMedUpcoming")
-    Collection<TestSession> findByTherapy_Med_UsernameAndDuration_StartTimeNotNullAndDuration_StartTimeGreaterThan(@Param("med") String username, @Param("currentDate") LocalDateTime currentDate);
+    Collection<TestSession> findByTherapy_Med_UsernameAndDuration_StartTimeNotNullAndDuration_StartTimeGreaterThan(@Param("med") String username, @Param("currentDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDateTime currentDate);
 
 }
